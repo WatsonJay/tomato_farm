9f0a1f59dfaea522656625ed7b73722b7bc2dcd9
WatsonJay <446725026@qq.com>
2019-11-19 11:26:10 +0800

    Create git_control.py

---
+++ main/util/git_control.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Jaywatson'
+
+import git
+import os
+
+class gitControl:
+    def __init__(self):
+        self.dirs = os.path.abspath("C:/Users/"+os.environ['USERNAME']+"/Documents/tomatoFarm/")
+        self.dataPath = self.dirs + "/data/"
+        self.token = "325ef5bcf63841202a017f86b7d772f78c4073f4"
+        if not os.path.exists(self.dataPath):
+            os.makedirs(self.dataPath)
+        self.url = "https://" + self.token + "@github.com/WatsonJay/tomato_farm_data.git"
+        self.fileName = "data.acc"
+        if not os.path.exists(self.dataPath+".git/"):
+            self.repo = git.Repo.clone_from(url=self.url,to_path=self.dataPath)
+            print("cloned")
+        else:
+            self.repo = git.Repo(self.dataPath)
+            print("pulled")
+        print(self.repo.is_dirty())
+        remote = self.repo.remote()
+        remote.pull()
+
+    def commitAndPush:
+        print("pushed")
+
+if __name__ == '__main__':
+    con = gitControl()
+    print("readed")
