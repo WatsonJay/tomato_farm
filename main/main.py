73aed26fabaae11987365581d546fb659d2f64ea
WatsonJay <446725026@qq.com>
2019-11-26 23:30:34 +0800

    update
    界面开发

--- main/main.py
+++ main/main.py
@@ -1,14 +1,5 @@
 import os
 
-from util.load_conf import config
-from util.logger import logger
-
 if __name__ == '__main__':
     if not os.path.exists("log"):
         os.makedirs("log")
-    log = logger()
-    mainlog = log.getlogger('root')
-    mainlog.warning("xxxxxxxxxxxxxxxxxxxxx")
-    con = config()
-    con.readConfig()
-    print("readed")
