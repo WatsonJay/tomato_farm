03f521ea08e9dbc87e990d554471e61070c7a7c6
WatsonJay <446725026@qq.com>
2019-12-05 00:34:20 +0800

    update
    1.制作界面
    2.添加图标
    3.修改git路径拼接

--- main/util/gitControl.py
+++ main/util/gitControl.py
@@ -11,13 +11,13 @@ class gitControl:
         #初始化参数配置，后期移至ini中加密存储
         self.dirs = os.path.abspath("C:/Users/"+os.environ['USERNAME']+"/Documents/tomatoFarm/")
         self.dataPath = self.dirs + "/data/"
-        self.token = "325ef5bcf63841202a017f86b7d772f78c4073f4"
         self.account = "WatsonJay"
         self.password = "cywqoa19941121"
+        self.programName = "tomato_farm_data"
         self.fileName = "data.db"
         if not os.path.exists(self.dataPath):
             os.makedirs(self.dataPath)
-        self.url = "https://" + self.account + ":" + self.password + "@github.com/WatsonJay/tomato_farm_data.git"
+        self.url = "https://" + self.account + ":" + self.password + "@github.com/"+ self.account +"/"+ self.programName +".git"
         #判断本地是否有git目录，存在既非初次运行
         if not os.path.exists(self.dataPath+".git/"):
             self.repo = git.Repo.clone_from(url=self.url,to_path=self.dataPath)
