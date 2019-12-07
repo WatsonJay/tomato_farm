a40a2ad462339b85eb3d7cebd6c604709f0f32ba
WatsonJay <446725026@qq.com>
2019-12-08 00:14:56 +0800

    update
    1.界面设计更新
    2.todo实现类
    3.增加图标

---
+++ main/UIImpls/todoWidgetImpl.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/7 22:05
+# @Author  : Jaywatson
+# @File    : todoWidgetImpl.py
+# @Soft    : tomato_farm
+
+from PyQt5 import QtCore
+from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QWidget
+from UI.todoWidget import Ui_todoWidget
+
+class todoWidgetImpl(QWidget, Ui_todoWidget):
+    # 初始化
+    def __init__(self, parent=None):
+        super(todoWidgetImpl, self).__init__(parent)
+        self.setupUi(self)
+        self.flag = False
+
+    # 无边框移动窗体
+    def mousePressEvent(self, QMouseEvent):
+        try:
+            if QMouseEvent.button() == QtCore.Qt.LeftButton:
+                self.flag = True
+                self.m_Position = QMouseEvent.globalPos() - self.pos()
+                QMouseEvent.accept()
+                self.setCusor(QCursor(QtCore.Qt.OpenHandCursor))
+        except Exception as e:
+            pass
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if QtCore.Qt.LeftButton and self.flag:
+            self.move(QMouseEvent.globalPos() - self.m_Position)
+            QMouseEvent.accept()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.flag = False
+        self.setCursor(QCursor(QtCore.Qt.ArrowCursor))
--- main/main.py
+++ main/main.py
@@ -7,13 +7,14 @@ import os
 import sys
 
 from PyQt5.QtWidgets import QApplication
-from UIImpls.mainWindowImpl import mainWindowImpl
+#from UIImpls.mainWindowImpl import mainWindowImpl
+from UIImpls.todoWidgetImpl import todoWidgetImpl
 
 if __name__ == '__main__':
     if not os.path.exists("log"):
         os.makedirs("log")
     #界面初始化
     app = QApplication(sys.argv)
-    mainWindow = mainWindowImpl()
+    mainWindow = todoWidgetImpl()
     mainWindow.show()
     sys.exit(app.exec_())
