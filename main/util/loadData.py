4d925ad33d1cb13eb6403a1d3bddd69f870d4dbf
WatsonJay <446725026@qq.com>
2019-12-09 23:54:06 +0800

    update
    1.引入sqlite

--- main/util/loadData.py
+++ main/util/loadData.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/12/1 22:43
 # @Author  : Jaywatson
-# @File    : gitControl.py
+# @File    : loadData.py
 # @Soft    : tomato_farm
+import sqlite3
+from util.logger import logger
+
+
+class sqlite:
+    def __init__(self,path):
+        self.path = path
+        log = logger()
+        self.confsql = log.getlogger('conf')
+        try:
+            self.conn = sqlite3.connect(self.path, timeout=5)
+            self.confsql.debug("连接成功")
+        except Exception as e:
+            self.confsql.error(e)
--- main/util/logger.py
+++ main/util/logger.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/12/1 22:43
 # @Author  : Jaywatson
-# @File    : gitControl.py
+# @File    : logger.py
 # @Soft    : tomato_farm
 import logging,os
 import logging.config
