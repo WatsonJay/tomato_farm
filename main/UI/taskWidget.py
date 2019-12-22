d795c1eaf767e308c3a0a2ba84e8d8b6c5050060
WatsonJay <446725026@qq.com>
2019-12-22 23:34:26 +0800

    update
    1.界面样式修改

--- main/UI/taskWidget.py
+++ main/UI/taskWidget.py
@@ -133,7 +133,249 @@ class Ui_taskWidget(object):
         self.verticalLayout_4.addWidget(self.TaskListWidget)
         self.stackedWidget.addWidget(self.page)
         self.page_2 = QtWidgets.QWidget()
+        self.page_2.setStyleSheet("QWidget>QLineEdit{\n"
+"    border:1px solid gray;\n"
+"    border-radius:10px;\n"
+"    padding:2px 4px;\n"
+"}\n"
+"QWidget>QDateEdit{\n"
+"    border:1px solid gray;\n"
+"    border-radius:10px;\n"
+"    padding:2px 4px;\n"
+"}\n"
+"QWidget>QDateEdit::drop-down{\n"
+"    width: 20px;\n"
+"    border-left-width: 1px;\n"
+"    border-left-style: solid;\n"
+"    border-left-color: gray; \n"
+"}\n"
+"QWidget>QDateEdit::down-arrow{\n"
+"    border-image:url(:/icon/down.png);\n"
+"}\n"
+"QWidget>QPushButton{\n"
+"    border:none;\n"
+"    color:white;\n"
+"    border-radius:12px;\n"
+"}\n"
+"QSpinBox,QDoubleSpinBox{\n"
+"    border:1px solid gray;\n"
+"    border-radius:10px;\n"
+"    padding:2px 4px;\n"
+"}\n"
+"\n"
+"/*spinbox 抬起样式*/\n"
+"QDoubleSpinBox::up-button,QSpinBox::up-button {\n"
+"    subcontrol-origin:border;\n"
+"    subcontrol-position:right;\n"
+"    image: url(:/icon/add.png);\n"
+"    width: 16px;\n"
+"    height: 20px;       \n"
+"}\n"
+"QDoubleSpinBox::down-button,QSpinBox::down-button {\n"
+"    subcontrol-origin:border;\n"
+"    subcontrol-position:left;\n"
+"    border-image: url(:/icon/remove.png);\n"
+"    width: 16px;\n"
+"    height: 17px;\n"
+"}\n"
+"/*按钮按下样式*/\n"
+"QDoubleSpinBox::up-button:pressed,QSpinBox::up-button:pressed{\n"
+"    subcontrol-origin:border;\n"
+"    subcontrol-position:right;\n"
+"    image: url(:/icon/add.png);\n"
+"    width: 16px;\n"
+"    height: 20px;       \n"
+"}\n"
+"  \n"
+"QDoubleSpinBox::down-button:pressed,QSpinBox::down-button:pressed{\n"
+"    subcontrol-position:left;\n"
+"    image: url(:/icon/remove.png);\n"
+"    width: 16px;\n"
+"    height: 17px;\n"
+"}")
         self.page_2.setObjectName("page_2")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout(self.page_2)
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.label_3 = QtWidgets.QLabel(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_3)
+        self.label_4 = QtWidgets.QLabel(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_4)
+        self.label_5 = QtWidgets.QLabel(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_5)
+        self.label_6 = QtWidgets.QLabel(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.LabelRole, self.label_6)
+        self.taskNameEdit = QtWidgets.QLineEdit(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.taskNameEdit.setFont(font)
+        self.taskNameEdit.setObjectName("taskNameEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.taskNameEdit)
+        self.label_7 = QtWidgets.QLabel(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_7.setFont(font)
+        self.label_7.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_7.setObjectName("label_7")
+        self.formLayout.setWidget(5, QtWidgets.QFormLayout.LabelRole, self.label_7)
+        self.taskDescEdit = QtWidgets.QLineEdit(self.page_2)
+        self.taskDescEdit.setMinimumSize(QtCore.QSize(0, 70))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.taskDescEdit.setFont(font)
+        self.taskDescEdit.setObjectName("taskDescEdit")
+        self.formLayout.setWidget(5, QtWidgets.QFormLayout.FieldRole, self.taskDescEdit)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.taskHourBox = QtWidgets.QSpinBox(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.taskHourBox.setFont(font)
+        self.taskHourBox.setMaximum(72)
+        self.taskHourBox.setObjectName("taskHourBox")
+        self.horizontalLayout_4.addWidget(self.taskHourBox)
+        self.label_9 = QtWidgets.QLabel(self.page_2)
+        self.label_9.setMaximumSize(QtCore.QSize(40, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_9.setFont(font)
+        self.label_9.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_9.setObjectName("label_9")
+        self.horizontalLayout_4.addWidget(self.label_9)
+        self.taskMinuteBox = QtWidgets.QSpinBox(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.taskMinuteBox.setFont(font)
+        self.taskMinuteBox.setMinimum(1)
+        self.taskMinuteBox.setMaximum(60)
+        self.taskMinuteBox.setObjectName("taskMinuteBox")
+        self.horizontalLayout_4.addWidget(self.taskMinuteBox)
+        self.label_8 = QtWidgets.QLabel(self.page_2)
+        self.label_8.setMaximumSize(QtCore.QSize(40, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.label_8.setFont(font)
+        self.label_8.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_8.setObjectName("label_8")
+        self.horizontalLayout_4.addWidget(self.label_8)
+        self.formLayout.setLayout(1, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_4)
+        self.taskDeadLineEdit = QtWidgets.QDateEdit(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.taskDeadLineEdit.setFont(font)
+        self.taskDeadLineEdit.setCalendarPopup(True)
+        self.taskDeadLineEdit.setObjectName("taskDeadLineEdit")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.taskDeadLineEdit)
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.yesRadioButton = QtWidgets.QRadioButton(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.yesRadioButton.setFont(font)
+        self.yesRadioButton.setObjectName("yesRadioButton")
+        self.horizontalLayout_5.addWidget(self.yesRadioButton)
+        self.noRadioButton = QtWidgets.QRadioButton(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.noRadioButton.setFont(font)
+        self.noRadioButton.setObjectName("noRadioButton")
+        self.horizontalLayout_5.addWidget(self.noRadioButton)
+        self.formLayout.setLayout(3, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_5)
+        self.stageSpinBox = QtWidgets.QSpinBox(self.page_2)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(10)
+        self.stageSpinBox.setFont(font)
+        self.stageSpinBox.setMinimum(1)
+        self.stageSpinBox.setMaximum(10)
+        self.stageSpinBox.setObjectName("stageSpinBox")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.stageSpinBox)
+        self.verticalLayout_7.addLayout(self.formLayout)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 198, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_7.addItem(spacerItem1)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem2)
+        self.commitButton = QtWidgets.QPushButton(self.page_2)
+        self.commitButton.setMinimumSize(QtCore.QSize(70, 24))
+        self.commitButton.setMaximumSize(QtCore.QSize(70, 24))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(9)
+        self.commitButton.setFont(font)
+        self.commitButton.setStyleSheet("QPushButton{\n"
+"    background:#6CE872;\n"
+"}\n"
+"QPushButton:hover{\n"
+"    background:#53b357;\n"
+"}\n"
+"QPushButton:pressed{\n"
+"    border:1px solid white;\n"
+"    background:#53b357;\n"
+"}")
+        self.commitButton.setObjectName("commitButton")
+        self.horizontalLayout_3.addWidget(self.commitButton)
+        self.cancleButton = QtWidgets.QPushButton(self.page_2)
+        self.cancleButton.setMinimumSize(QtCore.QSize(70, 24))
+        self.cancleButton.setMaximumSize(QtCore.QSize(70, 24))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(9)
+        self.cancleButton.setFont(font)
+        self.cancleButton.setStyleSheet("QPushButton{\n"
+"    background:#F95D5C;\n"
+"}\n"
+"QPushButton:hover{\n"
+"    background:#aa3e3e;\n"
+"}\n"
+"QPushButton:pressed{\n"
+"    border:1px solid white;\n"
+"    background:#aa3e3e;\n"
+"}")
+        self.cancleButton.setObjectName("cancleButton")
+        self.horizontalLayout_3.addWidget(self.cancleButton)
+        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem3)
+        self.verticalLayout_7.addLayout(self.horizontalLayout_3)
         self.stackedWidget.addWidget(self.page_2)
         self.verticalLayout_6.addWidget(self.stackedWidget)
         self.tabWidget.addTab(self.tab, "")
@@ -156,8 +398,8 @@ class Ui_taskWidget(object):
         self.verticalLayout_3 = QtWidgets.QVBoxLayout()
         self.verticalLayout_3.setContentsMargins(5, -1, 5, -1)
         self.verticalLayout_3.setObjectName("verticalLayout_3")
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_3.addItem(spacerItem1)
+        spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_3.addItem(spacerItem4)
         self.linkDayButton = QtWidgets.QPushButton(taskWidget)
         self.linkDayButton.setMinimumSize(QtCore.QSize(40, 40))
         self.linkDayButton.setMaximumSize(QtCore.QSize(40, 40))
@@ -200,8 +442,8 @@ class Ui_taskWidget(object):
         self.undoLinkButton.setIconSize(QtCore.QSize(20, 20))
         self.undoLinkButton.setObjectName("undoLinkButton")
         self.verticalLayout_3.addWidget(self.undoLinkButton)
-        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_3.addItem(spacerItem2)
+        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_3.addItem(spacerItem5)
         self.horizontalLayout_2.addLayout(self.verticalLayout_3)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
@@ -227,6 +469,7 @@ class Ui_taskWidget(object):
 
         self.retranslateUi(taskWidget)
         self.tabWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(taskWidget)
 
     def retranslateUi(self, taskWidget):
@@ -234,6 +477,19 @@ class Ui_taskWidget(object):
         self.addTaskButton.setToolTip(_translate("taskWidget", "新增"))
         self.modifTaskButton.setToolTip(_translate("taskWidget", "修改"))
         self.delTaskButton.setToolTip(_translate("taskWidget", "删除"))
+        self.label.setText(_translate("taskWidget", "任务名称："))
+        self.label_3.setText(_translate("taskWidget", "任务时长："))
+        self.label_4.setText(_translate("taskWidget", "结束期限："))
+        self.label_5.setText(_translate("taskWidget", "分阶段："))
+        self.label_6.setText(_translate("taskWidget", "阶段数："))
+        self.label_7.setText(_translate("taskWidget", "任务描述"))
+        self.label_9.setText(_translate("taskWidget", "小时"))
+        self.label_8.setText(_translate("taskWidget", "分钟"))
+        self.taskDeadLineEdit.setDisplayFormat(_translate("taskWidget", "yyyy-M-d"))
+        self.yesRadioButton.setText(_translate("taskWidget", "是"))
+        self.noRadioButton.setText(_translate("taskWidget", "否"))
+        self.commitButton.setText(_translate("taskWidget", "发布"))
+        self.cancleButton.setText(_translate("taskWidget", "取消"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("taskWidget", "任务箱"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("taskWidget", "历史任务"))
         self.linkDayButton.setToolTip(_translate("taskWidget", "添加到当日"))
