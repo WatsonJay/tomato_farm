e7b7aee2a9caa03b03fa3605cf95ffad2e3b53b0
WatsonJay <446725026@qq.com>
2020-01-08 17:40:44 +0800

    update
    1.追加图标
    2.功能更新

--- main/UIImpls/todayItemImpl.py
+++ main/UIImpls/todayItemImpl.py
@@ -3,33 +3,61 @@
 # @Author  : Jaywatson
 # @File    : todayItemImpl.py
 # @Soft    : tomato_farm
+
+from PyQt5 import QtGui
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QWidget
 from UI.todayListItem import Ui_todayListItem
+import UI.icons_rc
+from util.loadData import sqlite
+from util.logger import logger
+
 
 class todayItemImpl(QWidget, Ui_todayListItem):
 
     taskRefreshSignal = pyqtSignal()
-
     # 初始化
     def __init__(self, parent=None):
         super(todayItemImpl, self).__init__(parent)
         self.setupUi(self)
         self.taskId = ''
+        self.top = 0
+        log = logger()
+        self.conftodo = log.getlogger('gui')
+        self.sqlite = sqlite('./config/tomato.db')
+        self.upIcon = QtGui.QIcon()
+        self.upIcon.addPixmap(QtGui.QPixmap(":/icon/top.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.downIcon = QtGui.QIcon()
+        self.downIcon.addPixmap(QtGui.QPixmap(":/icon/untop.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.topButton.clicked.connect(self.topTask)
 
     # 信息填充
     def setInfo(self, data):
         if data['is_top'] == 1:
+            self.top = 1
             self.radiusLabel.setStyleSheet("#radiusLabel{border-radius:6px;background:#FEE66E;}")
+            self.topButton.setIcon(self.downIcon)
+        else:
+            self.top = 0
+            self.topButton.setIcon(self.upIcon)
+        self.taskId = data['id']
         self.taskNameLabel.setText(data['task_name'],"white")
 
     # 任务置顶
     def topTask(self):
         try:
-            sql = "Update t_task_link_date set is_doing = 0 where task_id = ?"
-            self.sqlite.execute(sql, self.task['id'])
+            if self.top == 1:
+                sql = "Update t_task_link_date set is_top = 0 where task_id = ?"
+                self.top = 0
+                self.topButton.setIcon(self.upIcon)
+                self.radiusLabel.setStyleSheet("#radiusLabel{border-radius:6px;background:white;}")
+            else:
+                sql = "Update t_task_link_date set is_top = 1 where task_id = ?"
+                self.top = 1
+                self.topButton.setIcon(self.downIcon)
+                self.radiusLabel.setStyleSheet("#radiusLabel{border-radius:6px;background:#FEE66E;}")
+            self.sqlite.execute(sql, self.taskId)
             self.taskRefreshSignal.emit()
             self.task = {}
         except Exception as e:
-            self.Tips("系统异常，请查看日志")
-            self.confmain.error(e)
+            self.conftodo.error(e)
